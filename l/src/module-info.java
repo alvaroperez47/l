ce8c7f88d98c25a7033e3aa8bb2e04b0bbc22127
ALVARO <ALVARO@DESKTOP-GN0JC4V>
2023-05-28 22:54:57 +0200

    a

---
+++ l/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ALVARO
+ *
+ */
+module l {
+}
---
+++ l/src/pkg/clase.java
@@ -0,0 +1,10 @@
+package pkg;
+
+public class clase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombre="a";
+	}
+
+}
